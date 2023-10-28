0bfb4c631a0cebad4fa7139c548cce09c49c34a8
JordanACB <carrionbarboza@gmail.com>
2023-10-28 18:10:06 -0500

    Listar Proveedor

---
+++ src/app/pages/orden-compra/lista-orden-compra/lista-orden-compra.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-orden-compra',
+  templateUrl: './lista-orden-compra.component.html',
+  styleUrls: ['./lista-orden-compra.component.css']
+})
+export class ListaOrdenCompraComponent {
+
+}
