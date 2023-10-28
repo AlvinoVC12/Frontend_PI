5384ee09183993e7020a4f3ee12694910d6b6575
yausin <fabrizioyp15@gmail.com>
2023-10-28 13:10:20 -0500

    nuevo producto

--- src/app/pages/producto/nuevo-producto/nuevo-producto.component.ts
+++ src/app/pages/producto/nuevo-producto/nuevo-producto.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Categoria } from 'src/app/modelo/Categoria';
+import { Producto } from 'src/app/modelo/Producto';
+import { CategoriaService } from 'src/app/services/categoria.service';
+import { ProductoService } from 'src/app/services/producto.service';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-nuevo-producto',
@@ -6,5 +13,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./nuevo-producto.component.css']
 })
 export class NuevoProductoComponent {
+  codigo:number
+  nombre:string
+  precio:number
+  stock:number
+  categoria:Categoria
+  codCategoria:number
 
+  listaCategorias:Categoria[]=[]
+
+  constructor(private apiCategoria:CategoriaService, private apiPro:ProductoService, private ruta:Router){}
+
+  ngOnInit(){
+    this.apiCategoria.getCategorias().subscribe(response=>{
+      this.listaCategorias=response
+    })
+  }
+
+  grabarDatos(){
+    this.categoria=new Categoria(this.codCategoria,"")
+    var objPro=new Producto(0,this.nombre,this.precio,this.stock,this.categoria)
+    this.apiPro.saveProducto(objPro).subscribe(response=>{
+      Swal.fire({
+        text: "Registro Exitoso",
+        icon: 'success',
+        confirmButtonColor: '#3085d6',
+      }).then((result) => {
+        if (result.isConfirmed) {
+          
+          this.ruta.navigate(["producto-lista"])
+        }
+      })
+    })
+  }
 }
